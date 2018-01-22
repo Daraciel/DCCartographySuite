25f068e7d799a57d5a5e069ff811100ec7fb93f3
Jorge Ponce <daraciel@gmail.com>
2018-01-23 00:45:20 +0100

    Added tetraedrical subdivision

---
+++ Core/Algorithm/WorldGen.Algorithm.TetrahedralSubdivision/TetrahedralSubdivision.cs
@@ -0,0 +1,29 @@
+﻿using WorldGen.Common.BE;
+using WorldGen.Common.Enums;
+using WorldGen.Common.Interfaces;
+
+namespace WorldGen.Algorithm.TetrahedralSubdivision
+{
+    public class TetrahedralSubdivision : IAlgorithm
+    {
+
+        #region IALGORITHM
+
+        public override IMap Create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Initialize(InitializeParams parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetParameter(AlgorithmParameters paramType, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
