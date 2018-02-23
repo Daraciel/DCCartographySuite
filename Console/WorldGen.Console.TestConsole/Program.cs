0452b10d56ff13c51c71453348af4976773e4b77
Jorge Ponce <jponce@equilibra.es>
2018-02-23 16:27:49 +0100

    Added Console project

---
+++ Console/WorldGen.Console.TestConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using WorldGen.Algorithm.TetrahedralSubdivision;
+using WorldGen.Common.BE;
+using WorldGen.Common.Enums;
+using WorldGen.Common.Maps;
+using WorldGen.Utilities.Logger;
+using WorldGen.Utilities.Enum;
+
+namespace WorldGen.Console.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TetrahedralSubdivision TSAlgorithm;
+            HeightMap TSMaps;
+            InitializeParams parameters;
+            double seed, width, height;
+            seed = 0.3;
+            width = 200;
+            height = 150;
+            TSAlgorithm = new TetrahedralSubdivision();
+            parameters = new InitializeParams();
+            parameters.Parameters.Add(Common.Enums.AlgorithmParameters.SEED, seed);
+            parameters.Parameters.Add(Common.Enums.AlgorithmParameters.WIDTH, width);
+            parameters.Parameters.Add(Common.Enums.AlgorithmParameters.HEIGHT, height);
+            parameters.Parameters.Add(Common.Enums.AlgorithmParameters.DEBUG, true);
+
+            StaticLogger.SetLoggerType(LoggerTypes.CONSOLE);
+            TSAlgorithm.Initialize(parameters);
+            TSMaps = (HeightMap)TSAlgorithm.Create();
+
+            TSMaps.SetColorSchema(@"ColorSchemas/Olsson.col");
+            //TSMaps.Save(@"C:/sample/sampleTSimage.jpg");
+        }
+    }
+}
