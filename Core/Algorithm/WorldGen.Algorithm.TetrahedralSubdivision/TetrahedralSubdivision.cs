e45b4e4d3d92ede152b431bdb14d3df609273742
Jorge Ponce Juan <es00656441@prosegur.com>
2026-01-29 18:32:57 +0100

    Mejoras en eficiencia y control de lados en Tetrahedron
    Se introduce control de recálculo de lados mediante la variable autoRecalculateSides y nuevos métodos UpdateA/B/C/D para actualizar vértices de forma eficiente. Se optimiza GetDistanceToPoint en Point3D y se ajusta la lógica de subdivisión para usar los nuevos métodos, evitando recálculos innecesarios. Actualizado efficiency-results.md con mejoras de rendimiento.

--- Core/Algorithm/WorldGen.Algorithm.TetrahedralSubdivision/TetrahedralSubdivision.cs
+++ Core/Algorithm/WorldGen.Algorithm.TetrahedralSubdivision/TetrahedralSubdivision.cs
@@ -1160,45 +1160,45 @@ namespace WorldGen.Algorithm.TetrahedralSubdivision
                     switch (longestSide)
                     {
                         case Enum.TetrahedronEdges.AB:
-                            tetra.B = E;
+                            tetra.UpdateB(E);
                             if(!tetra.IsInside(point))
                             {
-                                tetra.A = B;
+                                tetra.UpdateA(B);
                             }
                             break;
                         case Enum.TetrahedronEdges.AC:
-                            tetra.C = E;
+                            tetra.UpdateC(E);
                             if(!tetra.IsInside(point))
                             {
-                                tetra.A = B;
+                                tetra.UpdateA(B);
                             }
                             break;
                         case Enum.TetrahedronEdges.AD:
-                            tetra.D = E;
+                            tetra.UpdateD(E);
                             if(!tetra.IsInside(point))
                             {
-                                tetra.A = B;
+                                tetra.UpdateA(B);
                             }
                             break;
                         case Enum.TetrahedronEdges.BC:
-                            tetra.C = E;
+                            tetra.UpdateC(E);
                             if(!tetra.IsInside(point))
                             {
-                                tetra.B = B;
+                                tetra.UpdateB(B);
                             }
                             break;
                         case Enum.TetrahedronEdges.BD:
-                            tetra.D = E;
+                            tetra.UpdateD(E);
                             if(!tetra.IsInside(point))
                             {
-                                tetra.B = B;
+                                tetra.UpdateB(B);
                             }
                             break;
                         case Enum.TetrahedronEdges.CD:
-                            tetra.D = E;
+                            tetra.UpdateD(E);
                             if(!tetra.IsInside(point))
                             {
-                                tetra.C = B;
+                                tetra.UpdateC(B);
                             }
                             break;
                     }             
@@ -1279,10 +1279,10 @@ namespace WorldGen.Algorithm.TetrahedralSubdivision
                                 + es * this.AltitudeDifferenceWeight * Math.Pow(Math.Abs(tetra.A.Value - tetra.B.Value), this.AltitudeDifferencePower)
                                 + es1 * this.DistanceWeight * Math.Pow(longestSideValue, this.DistanceFunctionPower);
 
-                    tetra.B = E; 
+                    tetra.UpdateB(E);
                     if(!tetra.IsBeside(Enum.TetrahedronSides.BCD, point))
                     {
-                        tetra.A = B;
+                        tetra.UpdateA(B);
                     }
                              
                     result = this.getHeightForPoint(tetra, point, depth - 1);
